2ec555850183bb48c0bf57f4b3fd6db017967e7b
Shunya Matsuno <id.shma@gmail.com>
2016-01-07 10:23:48 +0900

    OSC複数台対応

--- bridge.js
+++ bridge.js
@@ -2,22 +2,12 @@ var osc = require('node-osc'),
     io = require('socket.io').listen(8081);
 
 var oscServer, oscClient;
+oscServer = new osc.Server(3333, "192.168.1.74");
+oscClient = new osc.Client("192.168.1.74", 3334);
 
 io.sockets.on('connection', function (socket) {
-  socket.on("config", function (obj) {
-    oscServer = new osc.Server(obj.server.port, obj.server.host);
-    oscClient = new osc.Client(obj.client.host, obj.client.port);
-
-    oscClient.send('/status', socket.sessionId + ' connected');
-
-    oscServer.on('message', function(msg, rinfo) {
-      console.log(msg, rinfo);
-      socket.emit("message", msg);
-    });
-  });
   socket.on("message", function (obj) {
-
       console.log(obj);
-    oscClient.send(obj);
+      oscClient.send(obj);
   });
 });
