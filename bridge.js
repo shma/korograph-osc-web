9d97444943e6c02869cd07602efca2ef06612c94
Shunya Matsuno <id.shma@gmail.com>
2016-01-07 10:24:47 +0900

    変数追加

--- bridge.js
+++ bridge.js
@@ -2,8 +2,10 @@ var osc = require('node-osc'),
     io = require('socket.io').listen(8081);
 
 var oscServer, oscClient;
-oscServer = new osc.Server(3333, "192.168.1.74");
-oscClient = new osc.Client("192.168.1.74", 3334);
+
+var iurl = "192.168.1.74";
+oscServer = new osc.Server(3333, iurl );
+oscClient = new osc.Client(iurl, 3334);
 
 io.sockets.on('connection', function (socket) {
   socket.on("message", function (obj) {
